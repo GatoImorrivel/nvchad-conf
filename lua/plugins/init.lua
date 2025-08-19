d1cba9147fb2c3eddd3476ddf46d5ebae04f3928
GatoImorrivel <guilhermevianagui23@gmail.com>
2025-08-19 14:12:05 -0300

    save

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,17 +13,32 @@ return {
     end,
   },
 
-	{
-		"ahmedkhalf/project.nvim",
+  {
+    "ahmedkhalf/project.nvim",
     event = "VeryLazy",
-		config = function()
-			require("project_nvim").setup({
-				patterns = { "Cargo.toml", ".git", "package.json", "tsconfig.json", "pnpm-workspace.yaml", ".stylua.toml" },
-				detection_methods = { "pattern" },
-				exclude_dirs = { ".cargo", "target", "node_modules", "dist" },
-			})
-		end,
-	},
+    config = function()
+      require("project_nvim").setup {
+        patterns = { "Cargo.toml", ".git", "package.json", "tsconfig.json", "pnpm-workspace.yaml", ".stylua.toml" },
+        detection_methods = { "pattern" },
+        exclude_dirs = { ".cargo", "target", "node_modules", "dist" },
+      }
+    end,
+  },
+
+  {
+    "nvim-tree/nvim-tree.lua",
+    config = function()
+      require("nvim-tree").setup {
+        renderer = {
+          root_folder_modifier = ":t", -- Show only the last part of the root folder
+        },
+        update_focused_file = {
+          enable = true, -- keeps tree in sync with the current file
+          update_root = true, -- updates tree root to the folder of current file
+        },
+      }
+    end,
+  },
 
   { import = "nvchad.blink.lazyspec" },
 
