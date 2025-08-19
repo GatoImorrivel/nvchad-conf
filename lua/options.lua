3f2d44d5047ae96ff6d345108c0f3004f9c96035
GatoImorrivel <guilhermevianagui23@gmail.com>
2025-08-19 16:08:39 -0300

    save

--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,16 @@ require "nvchad.options"
 -- add yours here!
 
 local o = vim.o
+
+require("telescope").setup{
+  defaults = {
+    file_ignore_patterns = {
+      ".git/",
+      "node_modules/",
+      ".turbo/",
+      "dist/",
+      "build/"
+    },
+  },
+}
+
